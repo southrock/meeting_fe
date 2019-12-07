61d21cb1fd8473e4cb660ab009628f0daac11083
southrock <southrock@foxmail.com>
2019-12-07 20:13:50 +0800

    :sparkles: new page login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import axios from 'axios';
+import { Button, Card, Form, Row, Input, Icon, message  } from 'antd';
+import { FormComponentProps, FormProps } from 'antd/es/form';
+
+const { useState } = React;
+
+interface FormType {
+  username: string;
+  password: string;
+}
+
+interface HandleSubmitSuccessProps {
+  name: string;
+}
+
+
+interface LoginFormProps extends FormComponentProps {
+  handleSubmitSuccess: (data: FormType) => void;
+}
+
+
+const LoginForm: React.FC<LoginFormProps> = (props) => {
+  const { getFieldDecorator } = props.form;
+
+  const handleSubmit:React.FormEventHandler = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        const { username,password } = props.form.getFieldsValue();
+        props.handleSubmitSuccess({username,password});
+      }
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Item>
+        {getFieldDecorator('username', {
+          rules: [{ required: true, message: 'Please input your username!' }]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="Username"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator('password', {
+          rules: [{ required: true, message: 'Please input your Password!' }]
+        })(
+          <Input
+            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            type="password"
+            placeholder="Password"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const WrappedFormComponent = Form.create<LoginFormProps>({ name: 'login_form' })( LoginForm );
+
+const Login = () => {
+
+  const handleSubmitSuccess = (data:FormType) => {
+    axios.post('http://localhost:8080/api/login',JSON.stringify(data),{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      const { data } = response;
+      const { status } = data;
+      if (status === 1) {
+        message.success(data.message);
+        // 切换页面 授权
+      }
+      else {
+        message.error(data.message);
+      }
+    });
+  };
+  return (
+    <div>
+      <Card>
+        <Row>
+          会议管理系统
+        </Row>
+        <WrappedFormComponent handleSubmitSuccess={handleSubmitSuccess} />
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
