e045124d0d60c0b84135e4c80410d40e776056da
southrock <southrock@foxmail.com>
2019-12-07 20:13:37 +0800

    :sparkles: new page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,56 +1,25 @@
 import * as React from 'react';
+import { Card, Row } from 'antd';
 
-const { useState } = React;
-
-interface FormType {
-  username: string;
-  password: string;
-  [propName: string]: string;
-}
+import './style.css';
 
 const Home = () => {
-
-  const [ formValues, setFormValues] = useState({
-    username: '',
-    password: ''
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-    const newValue: FormType = {...formValues};
-    newValue[name] = value;
-    setFormValues(newValue);
-  };
-
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-    e.persist();
-    e.preventDefault();
-  };
-
   return (
-    <div>
-      会议注册报名系统
-      <div>
-        <form action="" onSubmit={handleSubmit}>
-          <label htmlFor="username">账号：</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={formValues.username}
-            onChange={handleChange}
-          />
-          <label htmlFor="password">密码：</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={formValues.password}
-            onChange={handleChange}
-          />
-          <input type="submit" value="登录"/>
-        </form>
-      </div>
+    <div className="index-container">
+      <Card>
+        <Row>
+          我的会议
+        </Row>
+        <Row>
+          创建会议（organizer）
+        </Row>
+        <Row>
+          管理会议（admin）
+        </Row>
+        <Row>
+          设置要求（admin）
+        </Row>
+      </Card>
     </div>
   );
 };
