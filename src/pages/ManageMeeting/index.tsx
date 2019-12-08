06818f1fb66202851be659d6acefda8bb88a4639
southrock <southrock@foxmail.com>
2019-12-08 19:51:41 +0800

    :construction: 编辑会议功能和查看与会人员信息功能

--- src/pages/ManageMeeting/index.tsx
+++ src/pages/ManageMeeting/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Form, Table, Button, Popconfirm, Card, Row, message } from 'antd';
+import { Form, Table, Button, Popconfirm, Card, Row, message, Modal,Input, DatePicker } from 'antd';
 import { ColumnProps } from 'antd/es/table';
+import { FormComponentProps } from 'antd/es/form';
 import moment from 'moment';
 import axios from 'axios';
 
@@ -12,12 +13,72 @@ interface MeetingProp {
   time: number;
   position: string;
   requires: string[];
-  data: {};
+  data: {
+    [username: string]: {
+      [value: string]: string;
+    };
+  };
 }
+
+interface HandleSubmitSuccessProps {
+  name: string;
+}
+
+interface MeetingFormProps extends FormComponentProps {
+  record: MeetingProp;
+  // handleSubmitSuccess: (data: FormType) => void;
+  // allUsersArray: string[];
+  // allRequires: string[];
+}
+
+const FormComponent = React.forwardRef<FormComponentProps,MeetingFormProps>(({ form,record },ref) => {
+  React.useImperativeHandle(ref,() =>({
+    form,
+    record
+  }));
+
+  const { getFieldDecorator } = form;
+
+  return (
+    <Form>
+      <Form.Item label="会议名称">
+        {getFieldDecorator('name', {
+          rules: [{ required: true, message: '请输入会议名称!' }],
+          initialValue: record.name
+        })(
+          <Input />
+        )}
+      </Form.Item>
+      <Form.Item label="会议时间">
+        {getFieldDecorator('time', {
+          rules: [{ required: true, message: '请输入会议时间' }],
+          initialValue: moment(record.time)
+        })(
+          <DatePicker showTime />
+        )}
+      </Form.Item>
+      <Form.Item label="会议地点">
+        {getFieldDecorator('position', {
+          rules: [{ required: true, message: '请输入会议地点!' }],
+          initialValue: record.position
+        })(
+          <Input />
+        )}
+      </Form.Item>
+    </Form>
+  );
+});
+
+const WrappedFormComponent = Form.create<MeetingFormProps>({ name: 'meeting_form' })( FormComponent );
 
 const ManageMeeting: React.FC = () => {
 
   const [ data,setData ] = useState([]);
+  const [ nowRecord, setNowRecord ] = useState<MeetingProp>(undefined);
+  const [ formModalVisible,setFormModalVisible ] = useState<boolean>(false);
+  const [ tableModalVisible,setTableModalVisible ] = useState<boolean>(false);
+
+  const formRef = React.createRef<FormComponentProps>();
 
   useEffect(()=> {
     axios.get('http://localhost:8080/api/meetings')
@@ -48,7 +109,28 @@ const ManageMeeting: React.FC = () => {
     };
   };
 
-  const handleEdit = () => {
+  const handleSubmit = () => {
+    formRef.current.form.validateFields((err, values) => {
+      if (!err) {
+        const {
+          name,
+          time,
+          position
+        } = formRef.current.form.getFieldsValue();
+        console.log(formRef.current.form.getFieldsValue());
+        // props.handleSubmitSuccess({name,time,position,attenders,requires});
+      }
+    });
+  };
+
+  const handleEdit = (record:MeetingProp) => {
+    return () => {
+      setFormModalVisible(true);
+      setNowRecord(record);
+    };
+  };
+
+  const handleCheck = () => {
 
   };
 
@@ -85,13 +167,14 @@ const ManageMeeting: React.FC = () => {
             >
               <Button type="danger">删除</Button>
             </Popconfirm>
-            <Button onClick={handleEdit}>编辑</Button>
-            <Button onClick={handleEdit}>查看与会人员信息</Button>
+            <Button onClick={handleEdit(record)}>编辑</Button>
+            <Button onClick={() => {setTableModalVisible(true);setNowRecord(record)}}>查看与会人员信息</Button>
           </>
         );
       }
     }
   ];
+
 
   return (
     <div>
@@ -108,6 +191,27 @@ const ManageMeeting: React.FC = () => {
       <Row>
         要求管理
       </Row>
+      <Modal
+        title="修改会议信息"
+        visible={formModalVisible}
+        okText="提交"
+        onCancel={() => {setFormModalVisible(false)}}
+        onOk={handleSubmit}
+      >
+        <WrappedFormComponent wrappedComponentRef={formRef} record={nowRecord}/>
+      </Modal>
+      <Modal
+        title="与会人员信息"
+        visible={tableModalVisible}
+        okText="下载Excel表格"
+        onCancel={() => {setTableModalVisible(false)}}
+      >
+        <Table
+          columns={nowRecord ? [{title: '用户名',dataIndex:'username'}].concat(nowRecord.requires.map((v) => ({title: v,dataIndex: v}))) : [{title: '用户名',dataIndex:'username'}]}
+          dataSource={nowRecord ? Object.keys(nowRecord.data).map((k:string) => ({username:k,...nowRecord.data[k]})) : null}
+          rowKey="username"
+        />
+      </Modal>
     </div>
   );
 };
