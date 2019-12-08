5b3609aec5cdfea4b7eb5eef56d324d89471c407
southrock <southrock@foxmail.com>
2019-12-08 17:09:41 +0800

    :sparkles: 增加样板组件

---
+++ src/components/Template/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Form } from 'antd';
+
+const { useState, useEffect } = React;
+
+const Template: React.FC = () => {
+  return (
+    <div>
+      你好
+    </div>
+  );
+};
+
+export default Template;
