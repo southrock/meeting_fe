f3302f80b837a00864478d31ce9caabd756e9023
southrock <southrock@foxmail.com>
2019-12-11 21:56:29 +0800

    :wrench: 配置axios

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,8 @@ import * as ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.withCredentials=true;
 
 ReactDOM.render(<App />, document.getElementById('root'));
